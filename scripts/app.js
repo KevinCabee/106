c7f85fee6420b21a4f35d2637f5151003ec1b6c8
KevinCabee <kcabrera913@gmail.com>
2024-05-08 21:03:05 -0400

    Assignment 3

--- scripts/app.js
+++ scripts/app.js
@@ -1,30 +1,69 @@
 function saveTask() {
-    console.log("saving task");
+    console.log("saving tasks");
+}
     //get values
-    const title = $("#txtTitle").val();
-    const desc = $("#txtDescription").val();
-    const color = $("#selColor").val();
-    const date  = $("#selDate").val();
-    const status = $("#selStatus").val();
-    const budget = $("#numBudget").val();
-    console.log(title,desc,color,date,status,budget);
-
+    const title = $("#inputTitle").val();
+    const desc = $("#inputDescription").val();
+    const color = $("#inputColor").val();
+    const date = $("#inputDate").val();
+    const stat = $("#inputStatus").val();
+    const budget = $("#inputBudget").val();
+    console.log(title, desc, color, date, stat, budget);
     //build an object
-
+    let taskToSave = new Task (title, desc, color, date, stat, budget);
+    console.log(taskToSave);
     //save to server
-
+    $.ajax({
+        type: "POST",
+        url: "http://fsdiapi.azurewebsites.net/api/tasks/",
+        data: JSON.stringify(taskToSave),
+        contentType: "application/json",
+        success: function (res){
+            console.log(res);
+            displayTask(res);
+        },
+        error: function (error) {
+            console.log(error);
+        }
+    });
     //display task
+    function displayTask(task) {
 
-}
+        let syntax = `
+        <div class="task" style="border-color: ${task.color}">
+            <div class="info">
+                <h3>${task.title}</h3>
+                <p>${task.description}</p>
+            </div>
+            <label class="status">${task.stat}</label>
+            <div calss="date-budget">
+                <label>${task.date}</label>
+                <label>${task.budget}</label>
+            </div>
+        </div>
+        `;
+        $(".pending-task").append(syntax);
+    }
 
-function init() {
+    function testRequest() {
+        $.ajax({
+            type: "GET",
+            url: "http://fsdiapi.azurewebsites.net",
+            success: function (response) {
+                console.log(response);
+            },
+            error: function (error) {
+                console.log(error);
+            }
+        });
+    }
 
-    //load data
+    function init() {
+        //load data
+        //hook events
+        $("#btnSave").click(saveTask);//this is using jQuery
+    }
 
-    //hook events
-    $("#btnSave").click(saveTask);
-}
-
-window.onload = init();//avoid the init()
+    window.onload = init();//avoid the init()
 
 //validate a email regex
