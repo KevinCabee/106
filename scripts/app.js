92c177183988ea0f75860eaeda6138a00bc8a634
KevinCabee <kcabrera913@gmail.com>
2024-05-08 22:49:31 -0400

    Assignment 4

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,5 @@
 function saveTask() {
     console.log("saving tasks");
-}
     //get values
     const title = $("#inputTitle").val();
     const desc = $("#inputDescription").val();
@@ -18,32 +17,55 @@ function saveTask() {
         url: "http://fsdiapi.azurewebsites.net/api/tasks/",
         data: JSON.stringify(taskToSave),
         contentType: "application/json",
-        success: function (res){
-            console.log(res);
-            displayTask(res);
+        success: function (response){
+            console.log(response);
+            //displayTask(response);
+            //
         },
         error: function (error) {
             console.log(error);
         }
     });
     //display task
-    function displayTask(task) {
-
-        let syntax = `
-        <div class="task" style="border-color: ${task.color}">
-            <div class="info">
-                <h3>${task.title}</h3>
-                <p>${task.description}</p>
-            </div>
-            <label class="status">${task.stat}</label>
-            <div calss="date-budget">
-                <label>${task.date}</label>
-                <label>${task.budget}</label>
-            </div>
-        </div>
-        `;
-        $(".pending-task").append(syntax);
+}
+function displayTask(task) {
+    
+    let syntax = `
+    <div class="task" style="border-color: ${task.color}">
+    <div class="info">
+    <h3>${task.title}</h3>
+    <p>${task.description}</p>
+    </div>
+    <label class="status">${task.stat}</label>
+    <div calss="date-budget">
+    <label>${task.date}</label>
+    <label>${task.budget}</label>
+    </div>
+    </div>
+    `;
+    $(".pending-task").append(syntax);
+}
+    function loadTask(){
+        $.ajax({
+            type: "GET",
+            url: "http://fsdiapi.azurewebsites.net/api/tasks",
+            success: function(res) {
+                let data = JSON.parse(res);
+                console.log(data);
+                for(let i = 0; i < data.length; i++){
+                    let task = data[i];
+                    if(task.name == "Kevin")
+                    displayTask(task);
+                }
+                displayTask(data);
+                console.log(res);
+            },
+            error: function(error){
+                console.log(error);
+            }
+        });
     }
+
 
     function testRequest() {
         $.ajax({
@@ -62,6 +84,7 @@ function saveTask() {
         //load data
         //hook events
         $("#btnSave").click(saveTask);//this is using jQuery
+        loadTask();
     }
 
     window.onload = init();//avoid the init()
